1441fe93e3de1370c2aa9ff8afce41c32b40fdb2
veenahub <veena.pattigulla@gmail.com>
2020-03-25 16:00:22 +0100

    adding main

---
+++ main.java
@@ -0,0 +1,13 @@
+package PID1;
+
+public class main {
+	public static void main(String[] args)
+	{
+		System.out.print("hellp");
+		LinkedList list = new LinkedList();
+		list.add(5);
+		list.add(6);
+		System.out.println(list);
+	}
+
+}
